a3500def1157fa97d439226e37615bb41b8eff2a
jc <jiuqun.chan@365plam.com.tw>
2017-07-28 18:05:17 +0800

    更新程式庫

--- copy_debug.cmd
+++ copy_debug.cmd
@@ -1,17 +1,17 @@
 rd Assets\Project\Plugins\Backend /q /s
 mkdir Assets\Project\Plugins\Backend
 
-rem copy ..\ItIsNotAGame1-Backend\Game\bin\Debug\ Assets\Project\Plugins\Backend
-copy ..\ItIsNotAGame1-Backend\Data\bin\Debug\*.* Assets\Project\Plugins\Backend
-copy ..\ItIsNotAGame1-Backend\PlayUser\bin\Debug\*.* Assets\Project\Plugins\Backend
-rem copy ..\ItIsNotAGame1-Backend\PlayUser\bin\Debug\*.* Assets\Project\Plugins\Backend
+rem copy ..\ItIsNotAGame-Backend\Game\bin\Debug\ Assets\Project\Plugins\Backend
+copy ..\ItIsNotAGame-Backend\Data\bin\Debug\*.* Assets\Project\Plugins\Backend
+copy ..\ItIsNotAGame-Backend\PlayUser\bin\Debug\*.* Assets\Project\Plugins\Backend
+rem copy ..\ItIsNotAGame-Backend\PlayUser\bin\Debug\*.* Assets\Project\Plugins\Backend
 
 
 
 rd Assets\Project\RemotingCode\ /q /s
 mkdir Assets\Project\RemotingCode
 
-xcopy ..\ItIsNotAGame1-Backend\Game\*.cs Assets\Project\RemotingCode /s 
+xcopy ..\ItIsNotAGame-Backend\Game\*.cs Assets\Project\RemotingCode /s 
 
 
 
